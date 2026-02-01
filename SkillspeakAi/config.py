37078b2b99e91a6043036c0412b85d428ff6be20
pratham vernekar <153839232+prv05@users.noreply.github.com>
2026-02-01 14:10:06 +0530

    Delete SkillspeakAi directory

--- SkillspeakAi/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# MongoDB Connection
-# Default to local MongoDB. To use Atlas, set the MONGO_URI environment variable.
-MONGO_URI = os.getenv('MONGO_URI', 'mongodb://localhost:27017/skillspeak_ai')
-
-JWT_SECRET = os.getenv('JWT_SECRET', 'supersecretjwtkey')
-UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER', 'uploads/') 
